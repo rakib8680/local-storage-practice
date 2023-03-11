1e757487c5654825182fcc7ff572dedc53bf76bf
rakib8680 <rakib.cst.1st@gmail.com>
2023-03-11 20:01:01 +0600

    ok

--- index.js
+++ index.js
@@ -9,5 +9,35 @@ const counter = () => {
 const addNewNum = num => {
     let newNum = num + 1
     document.getElementById('number').innerText = newNum
+    saveDataToLocalStorage(newNum)
     console.log(newNum)
-}
+};
+
+
+const getDataFromLocalStorage = () => {
+    let number = 0;
+    const storedNum = localStorage.getItem('number');
+    if (storedNum) {
+        number = storedNum
+    }
+    return number;
+};
+
+
+const saveDataToLocalStorage = num => {
+    const numberString = getDataFromLocalStorage()
+    let number = parseInt(numberString);
+    const newNumber = number + 1
+    localStorage.setItem('number',newNumber)
+};
+
+
+
+const displayFromLocalStorage = () => {
+    const numberString = getDataFromLocalStorage();
+    const number = parseInt(numberString);
+    addNewNum(number);
+};
+
+displayFromLocalStorage()
+
